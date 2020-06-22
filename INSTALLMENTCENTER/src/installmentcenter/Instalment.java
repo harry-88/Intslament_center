49f3993565707f15d24fb9ac5b7d1789428eb12c
Muhammad Haris <67223517+harry-88@users.noreply.github.com>
2020-06-22 08:50:44 -0700

    Initial commit

---
+++ INSTALLMENTCENTER/src/installmentcenter/Instalment.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package installmentcenter;
+
+/**
+ *
+ * @author f2018266389
+ */
+public class Instalment {
+    private float monthly;
+    private float remaning;
+    private String date;
+
+    public float getMonthly() {
+        return monthly;
+    }
+
+    public void setMonthly(float monthly) {
+        this.monthly = monthly;
+    }
+
+    public float getRemaning() {
+        return remaning;
+    }
+
+    public void setRemaning(float remaning) {
+        this.remaning = remaning;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Instalment(float monthly, float remaning, String date) {
+        this.monthly = monthly;
+        this.remaning = remaning;
+        this.date = date;
+    }
+    
+}
